02b693b8a65f9c940a396a8caabf9e783f5fb1f0
Peter Graham <peterghm@gmail.com>
2013-05-27 11:12:08 -0700

    Resolve absolute URL for any given (relative or absolute) URL

--- content.js
+++ content.js
@@ -1,5 +1,20 @@
+var absoluteUrl = function(url, hostname) {
+  var base = $("base")[0];
+  if(!base) base = $("<base>").appendTo("head")[0];
+  base.href = hostname;
+
+  var resolver = document.createElement('a');
+  resolver.href = url;
+
+  return resolver.href;
+};
+
+var nextPage = function() {
+  return absoluteUrl($(".next_page:first").attr("href"), url('hostname'));
+};
+
 var prerender = function() {
-  var nextPageUrl = $(".next_page:first").attr("href");
+  var nextPageUrl = nextPage();
   if(!nextPageUrl) return;
   console.log("Prerendering: "+ nextPageUrl);
   $("<link rel='prerender' href='"+nextPageUrl+"'>").appendTo("head");
